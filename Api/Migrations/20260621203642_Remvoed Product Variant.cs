e89872bab39997cb6f281eb84a451f59bd1ae0a9
lukait11 <miljanovic.it11.2022@uns.ac.rs>
2026-06-21 22:38:17 +0200

    new migration

---
+++ Api/Migrations/20260621203642_Remvoed Product Variant.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemvoedProductVariant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImages_ProductVariants_ProductVariantId",
+                table: "ProductImages");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductVariantId",
+                table: "ProductImages",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImages_ProductVariants_ProductVariantId",
+                table: "ProductImages",
+                column: "ProductVariantId",
+                principalTable: "ProductVariants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImages_ProductVariants_ProductVariantId",
+                table: "ProductImages");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductVariantId",
+                table: "ProductImages",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImages_ProductVariants_ProductVariantId",
+                table: "ProductImages",
+                column: "ProductVariantId",
+                principalTable: "ProductVariants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Api/Models/Entities/Catalog/ProductImage.cs
+++ Api/Models/Entities/Catalog/ProductImage.cs
@@ -4,11 +4,9 @@ public class ProductImage
 {
   public Guid Id { get; set; }
   public Guid ProductId { get; set; }
-  public Guid ProductVariantId { get; set; }
   public string? ImageUrl { get; set; }
   public bool IsPrimary { get; set; }
   
   // Navigation properties
   public Product? Product { get; set; }
-  public ProductVariant? ProductVariant { get; set; }
 }
