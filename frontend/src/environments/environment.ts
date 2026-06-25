9ba44aacf7e56ce929f69b09b650ad9f3b4de08e
lukait11 <miljanovic.it11.2022@uns.ac.rs>
2026-06-26 01:06:03 +0200

    cors address changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://server1:8081/api',
+  apiUrl: 'http://188.2.46.198:8081/api',
   stripePublishableKey: 'pk_test_51TlTbzE3c1E5yhagShKI8RoJtabZXZO8eXBlj5XAKYGECoE4WwbI9GhKLYuhOrNu0VsBQKxfJNsjMjmuAYIQPVNL00BLQ1i030',
 };
